a09b90c9ff0fb8cc16dbe28eb2ce1588d6b4824f
NoumanMalik <noumanmalik3000@gmail.com>
2023-08-21 22:03:08 +0500

    Initial Code

---
+++ NumberGeneratorJob.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+
+namespace QuartzSample
+{
+    public class NumberGeneratorJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"Your # is: {RandomNumber(1000000, 999999999)}");
+            await Task.CompletedTask;
+        }
+
+        private int RandomNumber(int min, int max)
+        {
+            var random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
